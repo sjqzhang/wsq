cd6a04366cc2842dbcc038f09777ccb7bb0f3380
junqiang.zhang <junqiang.zhang@shopee.com>
2023-11-13 16:34:47 +0800

    add timeout

--- main.go
+++ main.go
@@ -457,6 +457,10 @@ func (h *hub) Subscribe(conn *Conn, subscription Subscription) {
 			if _, err := pipeliner.Exec(ctx); err == nil {
 				h.reqs.Store(reqKey, conn)
 				ts.AfterFunc(time.Second*60, func() {
+					conn.send <- WSMessage{
+						MessageType: websocket.TextMessage,
+						Data: response(-1,subscription,"request timeout"),
+					}
 					h.reqs.Delete(reqKey)
 				})
 			}
